ed70b18f62c7f3d5c0f11a071df5b686e6d43743
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-06-06 22:46:29 +0600

    shakil

--- src/Component/Blog/Blog.jsx
+++ src/Component/Blog/Blog.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { FaBookmark } from "react-icons/fa6";
 
 const Blog = (props) => {
     const {blog}=props;
     console.log(blog)
     return (
-        <div>
+        <div className='m-4 p-4'>
            {/* <h1>{blog.id}</h1>  */}
            <div className="card bg-base-100 w-96 shadow-sm">
                 <figure>
@@ -13,8 +14,18 @@ const Blog = (props) => {
                     alt="Shoes" />
                 </figure>
                 <div className="card-body">
+                    <div className="author flex justify-around items-center">
+                        <h3>{blog.author}</h3>
+                        <img className='w-16' src={blog.author_img} alt="" />
+                        <FaBookmark />
+                    </div> 
                     <h2 className="card-title">{blog.title}</h2>
                     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                    <div className="flex">
+                        {
+                       blog.hashtags.map((has)=><p>{has}</p>) 
+                    }
+                    </div>
                     <div className="card-actions justify-end">
                     <button className="btn btn-primary">Mark as read</button>
                     </div>
