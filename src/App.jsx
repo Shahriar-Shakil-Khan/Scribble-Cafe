1bb852660ae0665e5215d97e54182088a2e64d05
shahriar-shakil01797 <shahriarshakil28@gmail.com>
2025-06-06 19:57:10 +0600

    shakil

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import Blogs from './Component/Blogs/Blogs'
+import Navber from './Component/Navber/Navber'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <Navber></Navber>
+      <Blogs></Blogs>
+     
+    </>
+  )
+}
+
+export default App
